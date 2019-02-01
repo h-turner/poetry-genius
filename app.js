152dba0f265d679cf44f5d86092e4015bf348ee2
Chaiku <46286216+Chaiku@users.noreply.github.com>
2019-02-01 12:15:57 -0500

    working poetry display.

--- app.js
+++ app.js
@@ -1,43 +1,46 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-const authorName = "Shakespeare" //this will be = inputField.val()
+    const authorName = "Shakespeare" //this will be = inputField.val()
 
-const titlesURL = `http://poetrydb.org/author/${authorName}/title`;
+    const titlesURL = `http://poetrydb.org/author/${authorName}/title`;
 
-$.ajax({
-    url: titlesURL,
-    method: 'GET'
-}).then(function (response) {
-    let poemsArray = [];
-    for (let i = 0; i < response.length; i++) {
-        console.log(response[i].title)
-        let poemTitle = response[i].title;
-        $('.displayP').append(`<li class="titleP" id="${poemTitle}">${poemTitle}</li>`);
-        // $('.titleP').on("click", displayPoem());
-        poemsArray.push(poemTitle);
-        console.log(poemTitle.split(' ').join('%20'));
-        console.log(poemsArray);
+    $.ajax({
+        url: titlesURL,
+        method: 'GET'
+    }).then(function (response) {
+        for (let i = 0; i < response.length; i++) {
+            console.log(response[i].title)
+            let poemTitle = response[i].title;
+            $('.displayP').append(`<li class="titleP" data-poemTitle="${poemTitle}">${poemTitle}</li>`);
+        }
+        $('.titleP').on("click", displayPoem);
+    });
+
+
+    const displayPoem = function () {
+        let titleString = $(this).attr('data-poemTitle');
+        let urlString = titleString.split(' ').join('%20')
+        console.log(urlString);
+        const poemURL = `http://poetrydb.org/author,title/${authorName};${urlString}`
+        $.ajax({
+            url: poemURL,
+            method: 'GET'
+        }).then(function(response) {
+            console.log(response.lines);
+            // render ();
+            $('.displayP').empty();
+            for ( let i = 0; i < response[0].lines.length; i++) {
+            $('.displayP').append(`<p class="wordsDef">${response[0].lines[i]}</p>`)
+            }
+        })
+
     }
+
+    // const defineWord = function() {
+    //     let wordDef = $(this).attr('data-word')
+    // }
 });
 
-// const displayPoem = function() {
-
-// let titleString = $(this.id).split(' ').join('%20');//issue on this line... this.split doesn't work. We need to capture the ID of what's clicked.
-// const poemURL = `http://poetrydb.org/author,title/${authorName};${titleString}`
-//     $.ajax({
-//         url: poemURL,
-//         method: 'GET'
-//     }).then(function(response) {
-//         console.log(response.lines);
-//         // render ();
-//         $('.displayP').empty();
-//         $('.displayP').append(`<p>${response.lines}</p>`)
-//     })
-// }
-});
-// const render = function(displayPoem) {
-//     $('.displayP').empty();
-//     $('.displayP').append(`<p>${response.lines}`);
 
 
 //leftovers
